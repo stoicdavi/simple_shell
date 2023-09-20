a6d2e63e7bc72a08b5a7a116720fce04330be11e
David Nanjila <nanjiladavid2@gmai.com>
2023-09-20 22:38:01 +0300

    debugged the str_tok.c file

--- _str_tok.c
+++ _str_tok.c
@@ -1,21 +1,23 @@
 #include "shell.h"
 
 /**
- * _strtok - funtion to separate strings with delimiters
+ * _strtok - function to separate strings with delimiters
  * @line: the string to be separated.
- * @delim: to separate the strings.
+ * @delim: characters to use as delimiters.
  * Return: pointer to the created token
  */
 char *_strtok(char *line, char *delim)
 {
-	int a;
 	static char *str;
 	char *copystr;
+	int i = 0;
 
 	if (line != NULL)
 		str = line;
-	for (; *str != '\0'; str++)
+	while (*str != '\0')
 	{
+		int b;
+		str++;
 		for (b = 0; delim[b] != '\0'; b++)
 		{
 			if (*str == delim[b])
@@ -25,19 +27,22 @@ char *_strtok(char *line, char *delim)
 			break;
 	}
 	copystr = str;
+
 	if (*copystr == '\0')
 		return (NULL);
-	for (; *str != '\0'; str++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
+		int b;
 		for (b = 0; delim[b] != '\0'; b++)
 		{
-			if (*str == delim[b])
+			if (str[i] == delim[b]) 
 			{
-				*str = '\0';
+				str[i] = '\0';
 				str++;
 				return (copystr);
 			}
 		}
 	}
+
 	return (copystr);
 }
