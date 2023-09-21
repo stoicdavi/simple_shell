fc8f951a0c1f4bde163fd6b99f9dee542682cfbf
David Nanjila <nanjiladavid2@gmai.com>
2023-09-22 02:50:33 +0300

    shell

--- path.c
+++ path.c
@@ -2,7 +2,7 @@
 
 /**
  * find_command_path - searches through the directories to find a command
- *
+ * @command: command
  * Return: absolute path to the command if found, else NULL
  */
 char *find_command_path(char **command)
@@ -11,18 +11,17 @@ char *find_command_path(char **command)
 	char *path_dir = NULL;
 	char *cmd_path = NULL;
 
-	if (!command || !command[0]) {
-		return NULL;
-	}
-
+	if (!command || !command[0])
+		return (NULL);
 	if (access(command[0], F_OK) == 0)
-		return strdup(command[0]);
+		return (strdup(command[0]));
 
 	if (!path_var)
-		return NULL;
+		return (NULL);
 	path_dir = strtok(path_var, ":");
-	if (!path_dir) {
-		return NULL;
+	if (!path_dir)
+	{
+		return (NULL);
 	}
 	while (path_dir)
 	{
@@ -30,14 +29,13 @@ char *find_command_path(char **command)
 		strcpy(cmd_path, path_dir);
 		strcat(cmd_path, "/");
 		strcat(cmd_path, command[0]);
-
 		if (access(cmd_path, F_OK) == 0)
 		{
 			free(path_dir);
-			return cmd_path;
+			return (cmd_path);
 		}
 		free(cmd_path);
 		path_dir = strtok(NULL, ":");
 	}
-	return NULL;
+	return (NULL);
 }
